7caa21c76fb73fd41278d032600ca72ad1a8bf23
mart-dominguez <mdomingu@gmail.com>
2018-03-23 12:22:38 -0300

    doall

--- doall.sh
+++ doall.sh
@@ -1,7 +1,11 @@
+#mysql
+#sudo docker pull mysql
+#sudo docker run -d --net aepad --ip 172.18.0.2 -p 3336:3306 -e MYSQL_ROOT_PASSWORD=root --name tp-mysql mysql
+#sudo docker run -d --net aepad --ip 172.18.0.6 --name tp-myadmin --link tp-mysql:db -p 9001:80 phpmyadmin/phpmyadmin
 sudo docker kill cont-test-cloud
 sudo docker rm cont-test-cloud
 sudo docker rmi ams/test-cloud
 sudo docker build --no-cache -t ams/test-cloud .
-sudo docker run -d --net aepad --ip 172.18.0.10 -p 9010:8080 -p 9011:4848 --name cont-test-cloud ams/test-cloud
+sudo docker run -d --net aepad --ip 172.18.0.3 -p 9010:8080 --link tp-mysql:database --name cont-test-cloud ams/test-cloud
 #sudo docker logs cont-test-cloud
 
